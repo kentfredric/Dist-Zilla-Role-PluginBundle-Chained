d660a673c8cca595b5590fac5ec4776c636bdc3d
Kent Fredric <kentfredric@gmail.com>
2011-09-14 17:37:24 +1200

    committing first concept before gutting it and simplifying

--- lib/Dist/Zilla/Role/PluginBundle/Chained.pm
+++ lib/Dist/Zilla/Role/PluginBundle/Chained.pm
@@ -8,7 +8,6 @@ package Dist::Zilla::Role::PluginBundle::Chained;
 use Moose::Role;
 with 'Dist::Zilla::Role::PluginBundle';
 
-
 =head1 DESCRIPTION
 
 The point of this module is to provide an infrastructure for aggregating
@@ -56,9 +55,10 @@ And it should just behave as usual.
 However, you can then augment it with the following:
 
   sub chains {
-    return map {
-      'Dist::Zilla::Plugin::Chain::' . $_ 
-    } qw( Mangler OtherMangler );
+    return (
+      [ 'filter' , 'Dist::Zilla::Plugin::Chain::Filter' , [ @args ] ],
+      [ 'subargs', 'Dist::Zilla::Plugin::Chain::SubArgs', [ @otherargs ] ],
+    );
   }
 
 And now the parameters passed to the bundle are able to be pre-filtered/read
@@ -88,11 +88,152 @@ In this example, how this works should be similar to this code in a standard bun
 
 requires 'pluginbundle_config';
 
-sub bundle_config {
+sub chains {
+  return ();
+}
+
+sub _chained_load_class {
+  my ( $self, $class ) = @_;
+
+  # TODO put a shorthand expansion here.
+  require Class::Load;
+  Class::Load::load_class($class);
+  return $class;
+}
+
+sub _chained_name_from_class {
+  my ( $self, $chainmeta ) = @_;
+  return if exists $chainmeta->{name};
+  $chainmeta->{name} = $chainmeta->{instance}->name if $chainmeta->{instance}->can('name');
+  return if exists $chainmeta->{name};
+  my $name = $chainmeta->{classname};
+  $name =~ s/^Dist::Zilla::Plugin::Chain:://;
+  $name = lc($name);
+  $name =~ s/[^a-z0-9:]+/_/g;
+  $chainmeta->{name} = $name;
+  return;
+}
+
+sub _chained_vivify_scalar {
+  # Dist::Zilla::Plugin::Chain::Foo  => [ 'foo' , 'Dist::Zilla::Plugin::Chain::Foo' , [] ];
+  my ( $self, $classname ) = @_;
+  my $chainmeta = {};
+  $chainmeta->{passed_as} = 'scalar';
+  $chainmeta->{classname} = $self->_chained_load_class($chain);
+  $chainmeta->{instance}  = $chainmeta->{classname}->new();
+  $self->_chained_name_from_class($chainmeta);
+  return $chainmeta;
+}
+
+sub _chained_vivify_hashref {
+  my ( $self, $hashref ) = @_;
+  my $conf = { payload => [], };
+  $conf->{classname} = delete $hashref->{classname} if exists $hashref->{classname};
+  $conf->{name}      = delete $hashref->{name}      if exists $hashref->{name};
+  $conf->{payload}   = delete $hashref->{payload}   if exists $hashref->{payload};
+
+  for my $key ( keys %{$chain} ) {
+    warn "Key $key is not recognised as a parameter for HASH based chain entries.\n";
+  }
+  my $chainmeta = {};
+  $chainmeta->{passed_as} = 'hashref';
+  $chainmeta->{classname} = $self->_chained_load_class( $conf->{classname} );
+  $chainmeta->{payload}   = $conf->{payload};
+  $chainmeta->{instance}  = $chainmeta->{classname}->new( @{ $conf->{payload} } );
+  $chainmeta->{name}      = $conf->{name} if exists $conf->{name};
+  $self->_chained_name_from_class($chainmeta);
+  return $chainmeta;
+}
+
+sub _chained_vivify_name_array {
+  my ( $self, $array ) = @_;
+  my ( $name, $chain ) = @{$array};
+  my $chainmeta = $self->_chained_load_chain($chain);
+  $chainmeta->{passed_as} = 'arrayref[ name, ' . $chainmeta->{passed_as} . ']';
+  $chainmeta->{name}      = $name;
+  return $chainmeta;
+}
+
+sub _chained_vivify_triplet_array {
+  my ( $self, $array ) = @_;
+  my ( $name, $class, $payload ) = @{$array};
+  my $chainmeta = $self->_chained_load_chain(
+    {
+      name      => $name,
+      classname => $class,
+      payload   => $payload,
+    }
+  );
+  $chainmeta->{passed_as} = 'arrayref[ name, classname, payload ]';
+  return $chainmeta;
 
 }
 
+sub _chained_metafy_blessed {
+  my ( $self, $blessed ) = @_;
+  my $chainmeta = {};
+  $chainmeta->{passed_as} = 'bless';
+  $chainmeta->{classname} = Scalar::Util::blessed($blessed);
+  $chainmeta->{instance}  = $blessed;
+  $self->_chained_name_from_class($chainmeta);
+  return $chainmeta;
+}
 
+sub _chained_load_chain {
+  my ( $self, $chain ) = @_;
+
+  return $self->_chained_vivify_scalar($chain) if not ref $chain;
+  return $self->_chained_vivify_hashref($chain) if ref $chain eq 'HASH';
+  require Scalar::Util;
+  return $self->_chained_metafy_blessed($chain)       if Scalar::Util::blessed($chain);
+  return $self->_chained_vivify_name_array($chain)    if ( ref $chain eq 'ARRAY' and scalar @{$chain} == 2 );
+  return $self->_chained_vivify_triplet_array($chain) if ( ref $chain eq 'ARRAY' and scalar @{$chain} == 3 );
+
+  require Data::Dumper;
+  local $Data::Dumper::Indent   = 1;
+  local $Data::Dumper::Pad      = "   ";
+  local $Data::Dumper::Useqq    = 1;
+  local $Data::Dumper::Terse    = 1;
+  local $Data::Dumper::Sortkeys = 1;
+  die "Sorry, a chain element doesn't match any known parseable configuration type\n \$chain = " . Dumper($chain);
+
+  return;
+
+}
+
+sub _chained_pre_config {
+  my ( $self, $arg ) = @_;
+  my $state = {};
+
+  $state->{arg}     = $arg;
+  $state->{objects} = [ map { $self->_chained_load_chain($_) } $self->chains ];
+  $state->{result}  = undef;
+
+  for my $object ( @{ $state->{objects} } ) {
+    $object->pre_config($state);
+  }
+  return $state;
+}
+
+sub _chained_post_config {
+  my ( $self, $state );
+
+  for my $object ( reverse @{ $state->{objects} } ) {
+    $object->post_config($state);
+  }
+  return @{ $state->{result} };
+}
+
+sub bundle_config {
+  my ( $self, $arg ) = @_;
+
+  my ($state) = $self->_chained_pre_config($arg);
+
+  $state->{results} = [ $self->pluginbundle_config( $state->{arg} ) ];
+
+  return $self->_chained_post_config($state);
+
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
